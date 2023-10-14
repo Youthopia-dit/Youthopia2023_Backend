c376009c08ea78935e754d05747507711166b495
shreyal0007 <shreyaljain0007@gmail.com>
2023-10-14 12:42:11 +0530

    register api created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,34 @@
-//ndbnjdh
+const heads = require("../models/headModel");
+
+const registeruser = async (req, res) => {
+    const { user_id, team_name, team_members, event_id } = req.body;
+     try {
+    const existingUser = await heads.findOne({ user_id: user_id });
+    if (!existingUser) {
+      res.status(400).json({
+        status: "fail",
+        message: "User already exists",
+      });
+    }
+
+    const newDetail = new heads({
+      user_id: user_id,
+      team_name: team_name,
+      team_members: team_members,
+      event_id: event_id,
+      event_order_id: "dummy",
+    });
+
+    await newDetail.save();
+    res.status(200).json({
+      detail: newDetail,
+      message: "Registered successfully",
+    });
+     }
+
+     catch(err)  {
+        console.log(err)
+     }
+    }
+
+module.export={registeruser}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,1 +1,8 @@
-bnwsdhvwg
+const express = require("express");
+const userRouter = express.Router();
+const { registeruser } = require("../controllers/headController");
+const authenticateToken = require("../Middleware/authenticateToken");
+
+userRouter.post("/registeruser",authenticateToken , registeruser);
+
+module.exports = headRouter;
